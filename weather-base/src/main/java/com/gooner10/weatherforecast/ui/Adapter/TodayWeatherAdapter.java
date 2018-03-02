be77ed57dd5c6d658c80a6620d61bdb5314c6915
Birat Rai <biratrai@hotmail.com>
2018-03-01 23:15:50 -0500

    removing hungarian notation and commenting butterknife as it doesn't work with instant app

--- weather-base/src/main/java/com/gooner10/weatherforecast/ui/Adapter/TodayWeatherAdapter.java
+++ weather-base/src/main/java/com/gooner10/weatherforecast/ui/Adapter/TodayWeatherAdapter.java
@@ -15,38 +15,34 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
-import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class TodayWeatherAdapter extends RecyclerView.Adapter<TodayWeatherAdapter.ViewHolderData> {
     private final LayoutInflater layoutInflater;
-    private final Context mContext;
-    private final List<DailyTemp> mDailyTemp;
+    private final List<DailyTemp> dailyTemps;
     private int mPreviousPosition = 0;
 
-    public TodayWeatherAdapter(Context mContext, List<DailyTemp> mDailyTemp) {
-        layoutInflater = LayoutInflater.from(mContext);
-        this.mContext = mContext;
-        this.mDailyTemp = mDailyTemp;
+    public TodayWeatherAdapter(Context context, List<DailyTemp> dailyTemps) {
+        layoutInflater = LayoutInflater.from(context);
+        this.dailyTemps = dailyTemps;
     }
 
     @Override
     public ViewHolderData onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = layoutInflater.inflate(R.layout.daily_weather_row, parent, false);
-
         return new ViewHolderData(view);
     }
 
     @Override
     public void onBindViewHolder(final ViewHolderData holder, final int position) {
-        long mSystemMillisecond = Long.parseLong((mDailyTemp.get(position).getTime())) * 1000;
+        long mSystemMillisecond = Long.parseLong((dailyTemps.get(position).getTime())) * 1000;
         Date date = new Date(mSystemMillisecond);
         SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd");
         String mDailyDate = fmt.format(date);
 
-        holder.mDateTextView.setText(mDailyDate);
-        holder.mMinTempTextView.setText("Min Temperature: " + mDailyTemp.get(position).getApparentTemperatureMin());
-        holder.mMaxTempTextView.setText("Max Temperature: " + mDailyTemp.get(position).getApparentTemperatureMax());
+        holder.dateText.setText(mDailyDate);
+        holder.minTempText.setText("Min Temperature: " + dailyTemps.get(position).getApparentTemperatureMin());
+        holder.maxTempText.setText("Max Temperature: " + dailyTemps.get(position).getApparentTemperatureMax());
 
         boolean flag = (position > mPreviousPosition);
         RecyclerViewAnimUtils.animateSunblind(holder, flag);
@@ -56,24 +52,27 @@ public class TodayWeatherAdapter extends RecyclerView.Adapter<TodayWeatherAdapte
 
     @Override
     public int getItemCount() {
-        return mDailyTemp.size();
+        return dailyTemps.size();
     }
 
     public class ViewHolderData extends RecyclerView.ViewHolder {
         public final View mView;
 
-        @BindView(R.id.MinTempTextView)
-        TextView mMinTempTextView;
+//        @BindView(R.id.MinTempTextView)
+        TextView minTempText;
 
-        @BindView(R.id.DateTextView)
-        TextView mDateTextView;
+//        @BindView(R.id.DateTextView)
+        TextView dateText;
 
-        @BindView(R.id.MaxTempTextView)
-        TextView mMaxTempTextView;
+//        @BindView(R.id.MaxTempTextView)
+        TextView maxTempText;
 
         public ViewHolderData(View itemView) {
             super(itemView);
             mView = itemView;
+            minTempText = itemView.findViewById((R.id.MinTempTextView));
+            dateText = itemView.findViewById((R.id.DateTextView));
+            maxTempText = itemView.findViewById(R.id.MaxTempTextView);
             ButterKnife.bind(this, itemView);
         }
     }
