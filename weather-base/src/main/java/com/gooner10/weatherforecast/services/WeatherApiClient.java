8a31aa32c8c30af46a87877f8f06c8bff06d7315
Birat Rai <biratrai@hotmail.com>
2018-03-03 21:13:46 -0500

    adding retrofit client interface for get calls

---
+++ weather-base/src/main/java/com/gooner10/weatherforecast/services/WeatherApiClient.java
@@ -0,0 +1,14 @@
+package com.gooner10.weatherforecast.services;
+
+import com.gooner10.weatherforecast.model.pojo.ForeCastApiModel;
+
+import retrofit2.http.GET;
+
+/**
+ * Retrofit client for weather api
+ */
+
+public interface WeatherApiClient {
+    @GET
+    ForeCastApiModel getWeatherData();
+}
