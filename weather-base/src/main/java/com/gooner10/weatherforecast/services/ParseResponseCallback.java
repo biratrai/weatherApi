7e0af1ab1a5f5d07a88edea9fc886e80967cb3d3
Birat Rai <biratrai@hotmail.com>
2018-03-05 23:34:14 -0500

    removing volley methods

--- weather-base/src/main/java/com/gooner10/weatherforecast/services/ParseResponseCallback.java
+++
@@ -1,9 +0,0 @@
-package com.gooner10.weatherforecast.services;
-
-import org.json.JSONObject;
-
-public interface ParseResponseCallback {
-    void onSuccess(JSONObject response);
-
-    void onError(String errorMessage);
-}
--- weather-base/src/main/java/com/gooner10/weatherforecast/services/WeatherService.java
+++
@@ -1,5 +0,0 @@
-package com.gooner10.weatherforecast.services;
-
-public interface WeatherService {
-    void getWeatherData(ParseResponseCallback responseCallback);
-}
