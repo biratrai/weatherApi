93bbbf333482f47ca30cecc7b8cad9a20edaf330
Birat Rai <biratrai@hotmail.com>
2018-03-05 23:40:19 -0500

    moving the weatherhome package

---
+++ weather-base/src/main/java/com/gooner10/weatherforecast/weatherhome/WeatherContract.java
@@ -0,0 +1,28 @@
+package com.gooner10.weatherforecast.weatherhome;
+
+
+import com.gooner10.weatherforecast.model.pojo.DailyTemp;
+import com.gooner10.weatherforecast.model.pojo.ForeCastApiModel;
+
+import java.util.List;
+
+/**
+ * Contract for Weather
+ */
+public interface WeatherContract {
+
+    interface view {
+
+        void displayTodaysTemperature(ForeCastApiModel data);
+
+        void displayWeeklyWeatherData(List<DailyTemp> weeklyTemperature);
+    }
+
+    interface userActions {
+
+        void loadData();
+
+        void userClicksMoreInfoOnTodaysTemperature();
+
+    }
+}
