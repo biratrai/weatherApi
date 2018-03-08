39983617e51273c76f6182cc0b899cf036632f58
Birat Rai <biratrai@hotmail.com>
2018-03-07 22:08:30 -0500

    moving to app level

---
+++ weather-base/src/main/java/com/gooner10/weatherforecast/MyApplication.java
@@ -0,0 +1,22 @@
+package com.gooner10.weatherforecast;
+
+import android.app.Application;
+import android.content.Context;
+
+public class MyApplication extends Application {
+    private static MyApplication sInstance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        sInstance = this;
+    }
+
+    public static MyApplication getInstance() {
+        return sInstance;
+    }
+
+    public static Context getAppContext() {
+        return sInstance.getApplicationContext();
+    }
+}
